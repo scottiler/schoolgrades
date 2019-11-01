907bf88047b677d66d2b1752df932ca2406b9726
Perry Iler <piler1@my.westga.edu>
2019-11-01 09:14:17 -0400

    Added DropLowGradeStrategy with getComposite method

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/DropLowGradeStrategy.java
@@ -0,0 +1,30 @@
+package edu.westga.cs.schoolgrades.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javafx.collections.ObservableList;
+
+public class DropLowGradeStrategy implements GradingStrategy {
+	private GradingStrategy dropStrategy;
+	
+	public DropLowGradeStrategy(GradingStrategy dropStrategy) {
+		this.dropStrategy = dropStrategy;
+	}
+	
+	@Override
+	public double getCompositeValue(ObservableList<Grade> gradeValues) {
+		int index = 0;
+		
+		for(Grade current : gradeValues) {
+			if(gradeValues.get(index).getValue() < current.getValue()) {
+				index++;
+			}
+		}
+		
+		gradeValues.remove(index);
+		
+		return this.dropStrategy.getCompositeValue(gradeValues);
+	}
+
+}
