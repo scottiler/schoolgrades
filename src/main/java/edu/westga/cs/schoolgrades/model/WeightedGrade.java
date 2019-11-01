9f00f40260273bc75e6633a2f7f6f40429cd327b
Perry Iler <piler1@my.westga.edu>
2019-10-31 22:30:58 -0400

    Added WeighedGrade class with getValue method

---
+++ src/main/java/edu/westga/cs/schoolgrades/model/WeightedGrade.java
@@ -0,0 +1,18 @@
+package edu.westga.cs.schoolgrades.model;
+
+public class WeightedGrade implements Grade{
+
+	private double gradeValue;
+	private double weight;
+	
+	public WeightedGrade(double gradeValue, double weight) {
+		this.gradeValue = gradeValue;
+		this.weight = weight;
+	}
+	
+	@Override
+	public double getGrade() {
+		return this.gradeValue * this.weight;
+	}
+
+}
