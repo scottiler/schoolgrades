788e69d1b18ed5b77202c4755260869a37bcd5ab
Perry Iler <piler1@my.westga.edu>
2019-11-13 16:09:57 -0500

    Added HW and Exams containers

---
+++ src/main/java/edu/westga/cs/schoolgrades/controllers/SchoolGradesController.java
@@ -0,0 +1,16 @@
+package edu.westga.cs.schoolgrades.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SchoolGradesController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
